2a0cbfdade0c3d4627022ed7f0635972a725d713
Sutima Phetsatien <sutima.phe@gmail.com>
2023-10-30 01:19:14 +0700

    fix: deploy build failed

--- common/Magnetic/index.tsx
+++ common/Magnetic/index.tsx
@@ -14,11 +14,14 @@ export default function Index({
 
         magnetic.current?.addEventListener("mousemove", (e) => {
             const { clientX, clientY } = e;
-            const {height, width, left, top} = magnetic.current?.getBoundingClientRect();
-            const x = clientX - (left + width/2)
-            const y = clientY - (top + height/2)
-            xTo(x * 0.35);
-            yTo(y * 0.35)
+            const rect = magnetic.current?.getBoundingClientRect();
+            if (rect) {
+              const { height, width, left, top } = rect;
+              const x = clientX - (left + width / 2);
+              const y = clientY - (top + height / 2);
+              xTo(x * 0.35);
+              yTo(y * 0.35);
+            }
         })
         magnetic.current?.addEventListener("mouseleave", (e) => {
             xTo(0);
