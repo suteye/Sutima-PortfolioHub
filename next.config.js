40cec0a5ee6dbb5375c8025000affef4b55d2c54
Sutima Phetsatien <sutima.phe@gmail.com>
2023-10-30 19:24:34 +0700

    fix: try to rewrite url

--- next.config.js
+++ next.config.js
@@ -7,4 +7,24 @@ const nextConfig = {
     },
 }
 
+const getRedirects = async () => {
+    const redirects = [
+        // https://sutima-portfolio-hub-suteye.vercel.app/work/:id
+        {
+            source: '/work/:id',
+            destination: '/work/:id',
+        },
+        // https://sutima-portfolio-hub-suteye.vercel.app/work
+        {
+            source: '/work',
+            destination: '/work',
+        },
+        // https://sutima-portfolio-hub-suteye.vercel.app/about
+        {
+            source: '/about',
+            destination: '/about',
+        },
+    ]
+}
+
 module.exports = nextConfig
