6a8204261d0b0cb9bb851b11fd5c98c26cd9b742
Sutima Phetsatien <sutima.phe@gmail.com>
2023-10-30 22:52:56 +0700

    fix: error

--- app/work/[workId]/page.tsx
+++ app/work/[workId]/page.tsx
@@ -2,19 +2,3 @@ export default function Page({ params }: { params: { workId: string } }) {
   console.log(params.workId)
     return <div>My Work: {params.workId}</div>
   }
-
-  
-export async function getStaticPaths() {
-  // Fetch a list of dynamic paths from your data source.
-  const workIds = ['1', '2', '3']; // Replace with your actual work IDs
-
-  // Map the work IDs to an array of objects with `params` key.
-  const paths = workIds.map((workId) => ({
-    params: { workId },
-  }));
-
-  return {
-    paths,
-    fallback: false, // You can set this to 'true' or 'blocking' if you want to handle paths not defined in `workIds`.
-  };
-}
