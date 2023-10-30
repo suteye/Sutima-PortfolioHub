3e56b7ff4ac1b96a5316ec4b05b39db9c152c332
Sutima Phetsatien <sutima.phe@gmail.com>
2023-10-30 16:38:05 +0700

    fix: delete some config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
-const repo = "Sutima-PortfolioHub"
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
 const nextConfig = {
     output: "export",
     distDir: 'dist',
     images: {
         unoptimized: true,
     },
-    assetPrefix: assetPrefix,
-    basePath: basePath,
 }
 
 module.exports = nextConfig
